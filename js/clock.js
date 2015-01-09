d02450932107dfb3e64884e0d772140ffce81cf6
Garrett Squire <garrettsquire@gmail.com>
2015-01-09 15:34:22 -0800

    first commit

---
+++ js/clock.js
@@ -0,0 +1,24 @@
+/*
+ * author: Garrett Squire, gsquire
+ *
+ * A small function to return the current time to the screen.
+ *
+ */
+function getTime() {
+    // Execute the function every second.
+    window.setTimeout(getTime, 1000);
+
+    var cur_time = new Date();
+    var time_string = cur_time.getHours() + ":" + cur_time.getMinutes() + ":";
+    var seconds = cur_time.getSeconds();
+    if (seconds < 10) {
+        time_string += "0" + seconds;
+    } else {
+        time_string += seconds;
+    }
+
+    var clock = document.getElementById("g_clock");
+    clock.innerHTML = time_string;
+}
+
+window.onload = getTime();
