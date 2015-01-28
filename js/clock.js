fd5f1bed922a182210b6493431d004961a9c55c4
Garrett Squire <garrettsquire@gmail.com>
2015-01-27 21:06:47 -0800

    updated to include weather forecast, new background color, weather icon

--- js/clock.js
+++ js/clock.js
@@ -4,7 +4,7 @@
  * A small function to return the current time to the screen.
  *
  */
-'use strict';
+"use strict";
 
 function getTime() {
     // Execute the function every second.
@@ -23,4 +23,48 @@ function getTime() {
     clock.innerHTML = time_string;
 }
 
-window.onload = getTime();
+// Return the background color of a forecast given the temperature.
+function getForecastColor(temperature) {
+    var colorClass = "";
+
+    if (temperature < 60)
+        colorClass = "cold";
+    else if (temperature >= 60 && temperature < 70)
+        colorClass = "chilly";
+    else if (temperature >= 70 && temperature < 80)
+        colorClass = "nice";
+    else if (temperature >= 80 && temperature < 90)
+        colorClass = "warm";
+    else
+        colorClass = "hot";
+
+    return colorClass;
+}
+
+// Use the Forecast.io API to get the current weather information.
+function getTemp() {
+    var API_KEY = "146c4dcccbfcbf15a5c03790ae499447";
+    var lat = "35.300399"
+    var lon = "-120.662362"
+    var url = "https://api.forecast.io/forecast/" + API_KEY + "/" + lat + "," + lon;
+
+    $.ajax( {
+        url: url,
+        dataType: "jsonp",
+        success: function (forecastData) {
+            console.log(forecastData);
+            var image = "img/" + forecastData["daily"]["icon"] + ".png";
+            var bg_color = getForecastColor(forecastData["daily"]["data"][0]["temperatureMax"]);
+
+            $("#forecastLabel").html(forecastData["daily"]["summary"]);
+            $("#forecastIcon").attr("src", image);
+            $("body").addClass(bg_color);
+        }
+    } );
+}
+
+// Call the functions once the DOM loads.
+$(document).ready(function () {
+    getTime();
+    getTemp();
+} );
